fda45c576344d9a1dbba675d0086aaef80dfbbce
guoyanwei <guoyw02@missfresh.cn>
2018-06-18 15:52:49 +0800

    第二次提交

---
+++ testJava/src/HelloWord.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C), 2018-2018,missfresh
+ * FileName: HelloWord
+ * Author:   ywgguo
+ * Date:     2018/6/18 下午3:42
+ * Description: hello
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈hello〉
+ *
+ * @author ywgguo
+ * @create 2018/6/18
+ * @since 1.0.0
+ */
+public class HelloWord {
+
+
+    public static void main(String[] args){
+
+        //练习git
+        /**
+         * git clone https://github.com/18640869382/learningGit.git
+         * git branch develop
+         * git status
+         * git add testJava/src/HelloWord.java .idea/ learningGit.iml
+         * git commit -m "测试直接提交到develop"
+         * git push
+         *
+         *
+         */
+        System.out.print(" ");
+
+        System.out.print(" ");
+
+        System.out.print(" ");
+
+        System.out.print("  ");
+
+
+
+    }
+
+}
